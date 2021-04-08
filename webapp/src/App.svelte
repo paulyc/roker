39921da22d34ae5155263bda078c9c1fa699c738
paulyc <paulyc@quetzalcoatl.us>
2021-04-08 19:08:52 -0400

    Fix event handling and styling. Code cleanup. Physics bug.

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,29 +1,47 @@
+
 <script>
-	import ParamForm from './ParamForm.svelte';
+	import Params from './Params.svelte';
+	let params1,params2;
+	let constantPressure=true;
+	function update(params) {
+		if (constantPressure) {
+			params2.constantPressure(params.P_w, params.P_s, params.P_a);
+		}
+	}
 </script>
 
-<main>
-	<ParamForm/>
-</main>
-
 <style>
-	/*main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	@font-face {
+		font-family: 'Agave Nerd Font Mono';
+		font-style: monospace;
+		src: url(/agavenerdfontmono.ttf);
+	}
+	@font-face {
+		font-family: 'Operator Narrow';
+		font-style: normal;
+		src: url(/operator_narrow.woff2);
+	}
+	@font-face {
+		font-family: 'Operator Mono';
+		font-style: monospace;
+		src: url(/operator_mono.woff2);
+	}
+	main {
+		font-family: 'Agave Nerd Font Mono';
+	}
+	div {
+		/*max-width: 40%;*/
+		display: flex;
+		float:inline-start;
+	}
+	input {
+		vertical-align:center;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}*/
 </style>
+
+<main>
+	<div><Params bind:this={params1} on:update={e=>update(e.detail)}/></div>
+	<div><input type=checkbox bind:checked={constantPressure}>Constant Pressure</div>
+	<div><Params bind:this={params2} /></div>
+</main>
