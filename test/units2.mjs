dfcd7a094673c34904bc1d3c66590d5258ec469a
paulyc <paulyc@quetzalcoatl.us>
2021-07-11 10:59:24 -0400

    fixing units2

--- test/units2.mjs
+++ test/units2.mjs
@@ -8,13 +8,24 @@ function assertFail(fun){
 		assertTrue(e instanceof AssertionError);
 	}
 }
+function assertAll(...list) {
+	return list.reduce((p,c)=>p&&c,true);
+}
 export default function Test() {
-	assertTrue(1+1===2);
-	assertFail(()=>assertTrue(1+1!==2));
+	let a=Units.x(1);
+	assertTrue(a.val===1);
+	a=new Units.x(2);
+	assertTrue(a.val===2);
 	let x= Units.kilogram(13);
 	let y= Units.gram(x);
 	let z= Units.kilogram(y);
-	console.log(x,y,z);
-	console.log(...[x,y,z].map(v=>new String(v)));
+	console.log(x,`\n${x}\n`,y,`\n${y}\n`,z,`\n${z}`);
+	let $str='13 kilogram';
+	let $num=13;
+	assertAll(x===y,x===z,y===z);
+	assertAll(`${x}` === $str, +x===$num,x+''===$str,''+x===$str);
+	assertAll(`${z}` === $str, +z===$num,z+''===$str,''+z===$str);
+	$str='13000 gram';
+	$num=13;//value is in base units
+	assertAll(`${y}` === $str, +y===$num,y+''===$str,''+y===$str);
 }
-
